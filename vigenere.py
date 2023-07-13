9c6829efdf3a1d6fa5763852e6044fefdb66c966
Yeeka Yau <yyau@fu180067.fu.campus>
2023-07-13 09:28:58 -0400

    trying to fix latex

--- vigenere.py
+++ vigenere.py
@@ -11,7 +11,7 @@ import neural_network as nn
 
 st.title("Finding the Key Length of a Vigenere Cipher")
 
-st.sidebar.markdown("This small web application is based on the research project **_Finding the key length of a Vignere cipher_** at Furman University. The application allows you to either supply a piece of plain text along with a key, or a piece of Vigenere encrypted cipher text. The application then displays a number of statistics related to finding the key length of the cipher text.")
+st.sidebar.markdown("This small web application is based on the research project **_Finding the key length of a Vignere cipher_** at Furman University. The application allows you to either supply a piece of plain text along with a key, or a piece of Vigenere encrypted cipher text. Once input is supplied, the application then displays a number of statistics related to finding the key length of the cipher text.")
 
 intro_text2 = st.sidebar.markdown("Notably, some newer statistics from recent literature is included, as well as a neural network.")
 
@@ -40,14 +40,14 @@ if choice == "Encrypt my plain text with a given key":
 
 		st.markdown("The Index of Coincidence is the probability that two letters picked at random from a ciphertext are the same. It is defined as follows:")
 
-		st.latex(r'IC = \frac{1}{{l \choose 2}} \sum_{i = 1}^{26} {n_i \choose 2}')
+		st.latex(r'IC = \frac{1}{\\binom{l}{2}} \sum_{i = 1}^(26) \\binom{n_i}{2}')
 
 		st.latex(r'''where $n_i$ is the number of occurrences of the letter $i$ in the Ciphertext and $l$ is the length of the ciphertext.''')
 
 		st.markdown('**The Key Idea**')
 
 		bullet_points = [
-    					'In a piece of regular English text, $IC \approx 6.5\%$ (0.065).',
+    					'In a piece of regular English text, $IC \\approx 6.5\\%$ (0.065).',
     					'In the Vigenere cipher, $IC$ is lower because a single letter can be encrypted by different letters throughout the ciphertext (the goal of this cipher is so that frequency analysis cannot be used).',
     					'So the IOC generally tells you whether you have a monoalphabetic cipher or a polyalphabetic cipher.'
 			]
